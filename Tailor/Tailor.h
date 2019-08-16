3db367f03eba01bc3a83a13145997b2118c56199
Paulo F. Andrade <paulo@outercorner.com>
2019-08-16 16:00:16 +0100

    Initial Commit

---
+++ Tailor/Tailor.h
@@ -0,0 +1,19 @@
+//
+//  Tailor.h
+//  Tailor
+//
+//  Created by Developer on 16/08/2019.
+//  Copyright © 2019 Paulo Andrade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tailor.
+FOUNDATION_EXPORT double TailorVersionNumber;
+
+//! Project version string for Tailor.
+FOUNDATION_EXPORT const unsigned char TailorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tailor/PublicHeader.h>
+
+
